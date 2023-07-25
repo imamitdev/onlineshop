9ccaf335c8547707ff86479296ce3958e5ca5b79
amit singh <imamitsinghchauhan@gmail.com>
2023-07-25 23:50:07 +0530

    login and Registration

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,51 @@
+from typing import Any, Dict
 from .models import Account
 from django import forms
 
-class RegistrationFrom(forms.ModelForm):\
-    form=
+
+class RegistrationFrom(forms.ModelForm):
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={"placeholder": "Enter Password", "class": "form-control"}
+        )
+    )
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={"placeholder": "Enter Password", "class": "form-control"}
+        )
+    )
+
+    class Meta:
+        model = Account
+        fields = [
+            "first_name",
+            "last_name",
+            "email",
+            "phone_number",
+            "password",
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationFrom, self).__init__(*args, **kwargs)
+        self.fields["first_name"].widget.attrs[
+            "placeholder"
+        ] = "Enter First Name"
+        self.fields["last_name"].widget.attrs[
+            "placeholder"
+        ] = "Enter Last Name"
+        self.fields["email"].widget.attrs[
+            "placeholder"
+        ] = "Enter Email Address"
+        self.fields["phone_number"].widget.attrs[
+            "placeholder"
+        ] = "Enter Phone Number"
+        for field in self.fields:
+            self.fields[field].widget.attrs["class"] = "form-control"
+
+    def clean(self):
+        cleaned_data = super(RegistrationFrom, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError("Password does not match !")
